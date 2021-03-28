24e49226e20cb66ad752e48058d7c4a54aadedee
Elian Batista <elianbatista2000@gmail.com>
2021-03-27 22:10:04 -0300

    sign up first step

--- src/screens/SignUpFirstStep/styles.js
+++ src/screens/SignUpFirstStep/styles.js
@@ -14,33 +14,6 @@ export const ScrollerContainer = styled.View`
   padding: 20px;
 `
 
-export const HeaderSteps = styled.View`
-  flex-direction: row;
-  justify-content: space-between;
-`
-
-export const BoxIcon = styled.View`
-  flex: 3;
-  align-items: center;
-`
-
-export const IconText = styled.Text`
-  text-align: center;
-  margin-top: 6px;
-  color: ${props => props.active ? '#FF953C' : '#FFFFFF'} ;
-`
-
-export const BoxStepLine = styled.View`
-  flex: 2;
-`
-
-export const StepLine = styled.View`
-  height: 5px;
-  border-radius: 3px;
-  background-color: ${props => props.active ? '#FF953C' : '#FFFFFF'} ;
-  margin-top: 11px;
-`
-
 export const InputArea = styled.View`
   width: 100%;
   margin-top: 50px;
