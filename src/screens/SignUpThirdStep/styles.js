29463359f25a0d0f5d546da779f9ce6268de90a2
Elian Batista <elianbatista2000@gmail.com>
2021-03-28 22:11:33 -0300

    sign up third step

---
+++ src/screens/SignUpThirdStep/styles.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import styled from 'styled-components/native'
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  background-color: #444A5C;
+`
+
+export const Scroller = styled.ScrollView`
+
+`
+
+export const ScrollerContainer = styled.View`
+  padding: 20px;
+`
+
+export const InputArea = styled.View`
+  width: 100%;
+  margin-top: 50px;
+  margin-bottom: 50px;
+`
+
+export const HeaderText = styled.Text`
+  color: #FFFFFF;
+  text-align: center;
+  font-size: 14px;
+  line-height: 22px;
+  font-weight: bold;
+  margin-top: 40px;
+`
