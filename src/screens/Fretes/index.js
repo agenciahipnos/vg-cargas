d048dc3fbed5e3654cf01f16fef9d531d2f49fc5
Elian Batista <elianbatista2000@gmail.com>
2021-03-30 19:25:29 -0300

    screen frete

--- src/screens/Fretes/index.js
+++ src/screens/Fretes/index.js
@@ -1,14 +1,97 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
 
-const Fretes = () => {
+import {
+  Container,
+  Scroller,
+  Footer,
+  FreteContainer,
+  Menu,
+  MenuItem,
+  MenuItemText
+} from './styles'
+
+import HeaderUsuario from '../../components/HeaderUsuario'
+import FreteBox from '../../components/FreteBox'
+
+const Home = () => {
+  const [menuPress, setMenuPress] = useState(1)
+
   return (
-    <View>
-      <Text>
-        Fretes
-      </Text>
-    </View>
+    <Container>
+      <HeaderUsuario>
+        FRETES
+      </HeaderUsuario>
+      <Scroller>
+        <Menu>
+          <MenuItem 
+            style={{backgroundColor: menuPress === 1 ? '#FFA037' : '#FFFFFF'}}
+            onPress={() => setMenuPress(1)}
+          >
+            <MenuItemText
+              style={{color: menuPress === 1 ? '#FFFFFF' : '#222222'}}
+            >
+              Todos
+            </MenuItemText>
+          </MenuItem>
+          <MenuItem
+            style={{backgroundColor: menuPress === 2 ? '#FFA037' : '#FFFFFF'}}
+            onPress={() => setMenuPress(2)}
+          >
+            <MenuItemText
+              style={{color: menuPress === 2 ? '#FFFFFF' : '#222222'}}
+            >
+              Solicitados
+            </MenuItemText>
+          </MenuItem>
+        </Menu>
+        {
+          menuPress === 1 ?
+            <FreteContainer>
+              <FreteBox 
+                produto="Granito"
+              />
+              <FreteBox 
+                produto="Granito"
+              />
+              <FreteBox 
+                produto="Granito"
+              />
+              <FreteBox 
+                produto="Granito"
+              />
+              <FreteBox 
+                produto="Granito"
+              />
+              <FreteBox 
+                produto="Granito"
+              />
+              <FreteBox 
+                produto="Granito"
+              />
+              <FreteBox 
+                produto="Granito"
+              />
+              <FreteBox 
+                produto="Granito"
+              />
+              <FreteBox 
+                produto="Granito"
+              />
+              <FreteBox 
+                produto="Madeira"
+              />
+            </FreteContainer>
+          :
+          <FreteContainer>
+            <FreteBox 
+              produto="Teste"
+            />
+          </FreteContainer>
+        }
+        <Footer />
+      </Scroller>
+    </Container>
   )
 }
 
-export default Fretes
+export default Home
