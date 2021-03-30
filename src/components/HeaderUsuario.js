781b8ba7a90a1a0f1f331c78d24320f244359899
Elian Batista <elianbatista2000@gmail.com>
2021-03-29 21:35:07 -0300

    Header Usuario

---
+++ src/components/HeaderUsuario.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import styled from 'styled-components/native'
+
+import Person from '../assets/person.svg'
+import LogoIcon from '../assets/logo-icon.svg'
+
+const Container = styled.View`
+  flex-direction: row;
+  padding: 10px 20px;
+  background-color: #FFF;
+`
+
+const LogoContent = styled.View`
+  flex: 1;
+  justify-content: center;
+`
+
+const TitleContent = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`
+
+const PerfilIcon = styled.View`
+  flex: 1;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  position: relative;
+`
+
+const HeaderTitle = styled.Text`
+  text-align: center;
+  font-size: 18px;
+  font-weight: bold;
+`
+
+const NotificationContainer = styled.View`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  background-color: red;
+  width: 16px;
+  height: 16px;
+  border-radius: 8px;
+  align-items: center;
+  justify-content: center;
+`
+
+const NotificationNumber = styled.Text`
+  color: #fff;
+  font-size: 10px;
+`
+
+const HeaderUsuario = ({ children }) => {
+  return (
+    <Container>
+      <LogoContent>
+        <LogoIcon width="53" height="40" />
+      </LogoContent>
+      <TitleContent>
+        <HeaderTitle>
+          { children }
+        </HeaderTitle>
+      </TitleContent>
+      <PerfilIcon>
+        <Person width="43" height="43" /> 
+        <NotificationContainer>
+          <NotificationNumber>
+            2
+          </NotificationNumber>
+        </NotificationContainer>
+      </PerfilIcon>
+    </Container>
+  )
+}
+
+export default HeaderUsuario
