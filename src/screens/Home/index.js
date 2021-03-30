94c8bb9048973b2678fe99bed059d78e543e8071
Elian Batista <elianbatista2000@gmail.com>
2021-03-30 19:25:37 -0300

    home screen

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,54 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import {
   Container,
   Scroller,
-  Footer
+  Footer,
+  FreteContainer,
+  LocationArea,
+  LocationInput,
+  LocationFinder,
+  InfoFrete,
+  FreteText,
+  FreteIcon,
+  FreteIconText
 } from './styles'
 
 import HeaderUsuario from '../../components/HeaderUsuario'
 import FreteBox from '../../components/FreteBox'
 
+import MyLocationIcon from '../../assets/my_location.svg'
+
 const Home = () => {
+  const [locationText, setLocationText] = useState('')
+
   return (
     <Container>
       <HeaderUsuario>
         Olá, Vilcimar!
       </HeaderUsuario>
       <Scroller>
-        <FreteBox 
-          produto="Granito"
-        />
-        <FreteBox 
-          produto="Granito"
-        />
-        <FreteBox 
-          produto="Granito"
-        />
-        <FreteBox 
-          produto="Granito"
-        />
-        <FreteBox 
-          produto="Granito"
-        />
-        <FreteBox 
-          produto="Granito"
-        />
-        <FreteBox 
-          produto="Granito"
-        />
-        <FreteBox 
-          produto="Granito"
-        />
-        <FreteBox 
-          produto="Granito"
-        />
-        <FreteBox 
-          produto="Granito"
-        />
-        <FreteBox 
-          produto="Madeira"
-        />
+        <LocationArea>
+          <LocationFinder>
+            <MyLocationIcon width="24" height="24" fill="#FFF" />
+          </LocationFinder>
+          <LocationInput
+            placeholder="Onde você está?"
+            placeholderTextColor="#FFFFFF"
+            value={locationText}
+            onChangeText={(text) => setLocationText(text)}
+          />
+        </LocationArea>
+        <FreteContainer>
+          <InfoFrete>
+            <FreteText>
+              Confira alguns fretes disponíveis
+            </FreteText>
+            <FreteIcon>
+              <FreteIconText>
+                i
+              </FreteIconText>
+            </FreteIcon>
+          </InfoFrete>
+          <FreteBox 
+            produto="Granito"
+          />
+          <FreteBox 
+            produto="Granito"
+          />
+          <FreteBox 
+            produto="Granito"
+          />
+          <FreteBox 
+            produto="Granito"
+          />
+          <FreteBox 
+            produto="Granito"
+          />
+          <FreteBox 
+            produto="Granito"
+          />
+          <FreteBox 
+            produto="Granito"
+          />
+          <FreteBox 
+            produto="Granito"
+          />
+          <FreteBox 
+            produto="Granito"
+          />
+          <FreteBox 
+            produto="Granito"
+          />
+          <FreteBox 
+            produto="Madeira"
+          />
+        </FreteContainer>
         <Footer />
       </Scroller>
     </Container>
