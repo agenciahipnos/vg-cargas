42aec4e18f49254ee6e1f58647de9b0a4482736a
Elian Batista <elianbatista2000@gmail.com>
2021-03-29 21:35:26 -0300

    style home screen

---
+++ src/screens/Home/styles.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components/native'
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+`
+
+export const Scroller = styled.ScrollView`
+  flex: 1;
+  padding: 0 20px;
+  padding-top: 20px;
+`
+
+export const Footer = styled.View`
+  height: 20px;
+`
