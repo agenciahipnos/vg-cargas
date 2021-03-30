106078085d3bd65ac764c3bdffabf22d713b359a
Elian Batista <elianbatista2000@gmail.com>
2021-03-29 21:35:43 -0300

    Opcoes Screen

---
+++ src/screens/Opcoes/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Opcoes = () => {
+  return (
+    <View>
+      <Text>
+        Opções
+      </Text>
+    </View>
+  )
+}
+
+export default Opcoes
