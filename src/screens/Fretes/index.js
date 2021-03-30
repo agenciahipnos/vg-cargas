4c8c9800cb2e639b70b287099d32b5ca94da3ae9
Elian Batista <elianbatista2000@gmail.com>
2021-03-29 21:35:13 -0300

    Frete index screen

---
+++ src/screens/Fretes/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Fretes = () => {
+  return (
+    <View>
+      <Text>
+        Fretes
+      </Text>
+    </View>
+  )
+}
+
+export default Fretes
